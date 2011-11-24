b66bcebf64c7c422f55713fdfaeedd8daf44d61a
Jookia <166291@gmail.com>
2011-11-25 04:38:41 +1100

    Redid Python3 stuff to be in the form of an environmental script that's not Python3 only, and a single patch.

--- src/third_party/patches/depot_tools_python3.sh
+++ src/third_party/patches/depot_tools_python3.sh
@@ -1,25 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-PYTHON2="python"
+if [ ! -d "$PWD/../depot_tools" ]; then
+	echo "Run this from the patches directory!" 1>&2
+	exit
+fi
+
+# /usr/bin/python is explicity used in depot_tools, and we're fixing that.
+PYTHON="/usr/bin/python"
 PYTHONVER="`$PYTHON2 --version 2>&1`"
 
-# Check if $PYTHON2 is Python 2..
+# Check if PYTHON is Python 2..
 if [ "`echo \"$PYTHONVER\" | grep \"Python 2\"`" == "" ]; then # It's not!
-	PYTHON2="python2" # A good guess..
+	# Clever hack borrowed from the official Arch Linux Chromium PKGBUILD.
+	# The idea is that shebangs use absolute paths, which need to be changed.
+	# Relative calls like 'python blah.py' get hijacked by the 'python' in $PATH.
 	
-	if [ "`which $PYTHON2`" != "" ]; then # Will be "" if $PYTHON2 isn't found.
-		cd depot_tools
-		
-		# Clever hacks borrowed from the official Arch Linux Chromium PKGBUILD.
-		
-		find . -type f -exec sed -i -r \
-			-e 's|/usr/bin/python$|&2|g' \
-			-e 's|(/usr/bin/python2)\.4$|\1|g' \
-			{} +
-		
-		cd .. 
-		
-		mkdir "python2-path"
-		ln -s `which $PYTHON2` "python2-path/python"
-	fi
+	REALPYTHON="$(readlink `which python`)"
+	
+	cd ../depot_tools
+	
+	find . -type f -exec sed -i -r \
+		-e "s|/usr/bin/python$|$REALPYTHON|g" \
+		-e "s|/usr/bin/python2\.4$|$REALPYTHON|g" \
+		{} +
+else
+	echo "`which $PYTHON` is Python 2, no need to patch." 1>&2
 fi
--- src/third_party/python_env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export PATH="$PWD/python2-path:$PATH:$PWD/depot_tools"
-export PYTHONPATH=$PYTHONPATH:$PWD/gyp/pylib/
