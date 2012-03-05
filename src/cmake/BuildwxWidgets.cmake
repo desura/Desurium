26e725c60945853121fdb357573547064edc06aa
Karol Herbst <git@karolherbst.de>
2012-03-05 17:31:17 +0100

    fix install path for wxWidgets

--- src/cmake/BuildwxWidgets.cmake
+++ src/cmake/BuildwxWidgets.cmake
@@ -12,5 +12,11 @@ set(wxWidgets_INSTALL_DIR ${CMAKE_SOURCE_DIR}/third_party/wxWidgets)
 set(wxWidgets_BIN_DIRS ${wxWidgets_INSTALL_DIR}/bin)
 set(wxWidgets_LIBRARY_DIRS ${wxWidgets_INSTALL_DIR}/lib)
 set(wxWidgets_INCLUDE_DIRS ${wxWidgets_INSTALL_DIR}/include/wx-2.9-desura ${wxWidgets_LIBRARY_DIRS}/wx/include/gtk2-unicode-release-2.9-desura)
-set(wxWidgets_LIBRARIES "${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so;${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura_gl-2.9.so")
-install(FILES "${wxWidgets_LIBRARIES}" DESTINATION ${LIB_INSTALL_DIR})
+set(wxWidgets_LIBRARIES "${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0.0.0;${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura_gl-2.9.so.0.0.0")
+install(FILES ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so
+              ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0
+              ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0.0.0
+              ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura_gl-2.9.so
+              ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura_gl-2.9.so.0
+              ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura_gl-2.9.so.0.0.0
+        DESTINATION ${LIB_INSTALL_DIR})
