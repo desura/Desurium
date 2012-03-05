e805fcd2af648677d2f1737b9dabec91fc29926e
Karol Herbst <git@karolherbst.de>
2012-03-06 00:35:16 +0100

    add README.cmake

---
+++ README.cmake
@@ -0,0 +1,12 @@
+easy way to build with cmake:
+
+run ./build_cmake.sh
+
+for faster build you can pass options for make
+
+eg: 
+./build_cmake.sh -j8
+
+to clear all files:
+
+run ./build_cmake_clean.sh
