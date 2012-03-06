9cd758292854bfd79518bc5624d25759217bb09c
Karol Herbst <git@karolherbst.de>
2012-03-06 15:00:18 +0100

    remove OpenGL from wxWidgets to fix bug and speed up compile
    fixes #4

--- cmake/BuildwxWidgets.cmake
+++ cmake/BuildwxWidgets.cmake
@@ -4,7 +4,7 @@ ExternalProject_Add(
     UPDATE_COMMAND ""
     PATCH_COMMAND patch -p0 -N -i ${CMAKE_SOURCE_DIR}/src/third_party/patches/wxWidgets_patch.patch
     CONFIGURE_COMMAND <SOURCE_DIR>/configure --enable-shared --enable-unicode
-        --enable-monolithic --with-flavour=desura --disable-threads
+        --enable-monolithic --with-flavour=desura --disable-threads --with-opengl=no
         --disable-joystick --disable-mediactrl --prefix=${CMAKE_SOURCE_DIR}/src/third_party/wxWidgets
 )
 
@@ -13,19 +13,9 @@ set(wxWidgets_BIN_DIRS ${wxWidgets_INSTALL_DIR}/bin)
 set(wxWidgets_LIBRARY_DIRS ${wxWidgets_INSTALL_DIR}/lib)
 set(wxWidgets_INCLUDE_DIRS ${wxWidgets_INSTALL_DIR}/include/wx-2.9-desura ${wxWidgets_LIBRARY_DIRS}/wx/include/gtk2-unicode-release-2.9-desura)
 
-if(OPENGL_FOUND)
-  set(wxWidgets_LIBRARIES "${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0.0.0;${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura_gl-2.9.so.0.0.0")
-  install(FILES ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so
-                ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0
-                ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0.0.0
-                ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura_gl-2.9.so
-                ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura_gl-2.9.so.0
-                ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura_gl-2.9.so.0.0.0
-          DESTINATION ${LIB_INSTALL_DIR})
-else()
-  set(wxWidgets_LIBRARIES "${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0.0.0")
-  install(FILES ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so
-                ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0
-                ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0.0.0
-          DESTINATION ${LIB_INSTALL_DIR})
-endif()
+set(wxWidgets_LIBRARIES "${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0.0.0")
+install(FILES ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so
+              ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0
+              ${wxWidgets_LIBRARY_DIRS}/libwx_gtk2u_desura-2.9.so.0.0.0
+        DESTINATION ${LIB_INSTALL_DIR})
+#endif()
