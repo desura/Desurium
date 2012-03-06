48ad215e5a4d44fce9d5e80303659378fda2ba78
Karol Herbst <git@karolherbst.de>
2012-03-06 10:13:15 +0100

    some shell scripts

--- build_cmake.sh
+++ build_cmake.sh
@@ -2,4 +2,4 @@
 mkdir build
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../install
-make $*
+make install $*
---
+++ run_desura.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=`pwd`/install/lib
+cd install/bin
+./Desurium
