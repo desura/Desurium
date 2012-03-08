b4ba6651527d789e041c1640b2d587a2fba59c3c
Karol Herbst <git@karolherbst.de>
2012-03-08 14:09:42 +0100

    no insource build for cURL

--- cmake/modules/BuildCURL.cmake
+++ cmake/modules/BuildCURL.cmake
@@ -1,8 +1,10 @@
+set(CURL_INSTALL_DIR ${CMAKE_EXTERNAL_BINARY_DIR}/curl)
+
 ExternalProject_Add(
     curl
     SOURCE_DIR ${CMAKE_SOURCE_DIR}/src/third_party/curl
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND sh ${CMAKE_SOURCE_DIR}/src/third_party/curl_build/configure
+    CONFIGURE_COMMAND sh ${CMAKE_BINARY_DIR}/curl/configure
         --without-librtmp --disable-ldap --disable-debug --disable-curldebug
         --without-zlib --disable-rtsp --disable-manual --enable-static=yes 
         --enable-shared=no --disable-pop3 --disable-imap --disable-dict
@@ -11,13 +13,13 @@ ExternalProject_Add(
         --without-nss --without-cyassl --with-ssl --without-axtls
         --without-libssh2 --enable-hidden-symbols --enable-cookies --without-sspi
         --disable-manual --enable-optimize=-O2+ --enable-ares
-        --prefix=${CMAKE_SOURCE_DIR}/src/third_party/curl_install
+        --prefix=${CURL_INSTALL_DIR}
 )
 
 ExternalProject_Add_Step(
     curl
     copyProject
-    COMMAND cp -r ${CMAKE_SOURCE_DIR}/src/third_party/curl ${CMAKE_SOURCE_DIR}/src/third_party/curl_build
+    COMMAND cp -r ${CMAKE_SOURCE_DIR}/src/third_party/curl ${CMAKE_BINARY_DIR}/curl
 )
 
 ExternalProject_Add_Step(
@@ -26,10 +28,9 @@ ExternalProject_Add_Step(
     COMMAND sh buildconf
     DEPENDEES copyProject
     DEPENDERS configure
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/src/third_party/curl_build
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/curl
 )
 
-set(CURL_INSTALL_DIR ${CMAKE_SOURCE_DIR}/src/third_party/curl_install)
 set(CURL_BIN_DIRS ${CURL_INSTALL_DIR}/bin)
 set(CURL_LIBRARY_DIR ${CURL_INSTALL_DIR}/lib)
 set(CURL_INCLUDE_DIRS ${CURL_INSTALL_DIR}/include)
