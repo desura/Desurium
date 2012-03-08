a84e66e5c3e537aa6b4d55c55c23fbc3e95dd056
Karol Herbst <git@karolherbst.de>
2012-03-08 11:12:39 +0100

    added two cmake macros
    seperate Debug and Release flags for builds
    
    force Debug build, because Release will copy binary files

--- cmake/platform/unix/platform.cmake
+++ cmake/platform/unix/platform.cmake
@@ -2,22 +2,29 @@
 # some global defines
 ###############################################################################
 
-add_definitions(-DDEBUG -DBOOST_FILESYSTEM_VERSION=2 -D_LARGE_FILES
+add_definitions(-DBOOST_FILESYSTEM_VERSION=2 -D_LARGE_FILES
     -D_FILE_OFFSET_BITS=64)
 
 ###############################################################################
 # some compiler flags
 ###############################################################################
 
-add_definitions(-fPIC -pipe -g3 -fvisibility=hidden -rdynamic
-    -fno-omit-frame-pointer -Wl,-Bsymbolic-functions -lpthread)
+if(CMAKE_BUILD_TYPE MATCHES "Debug")
+    add_definitions(-DDEBUG)
+else()
+    add_definitions(-DNDEBUG)
+endif()
+
+add_compiler_flags(-fPIC -pipe -fvisibility=hidden -Wl,-Bsymbolic-functions -lpthread -g3)
+add_compiler_flags(DEBUG -rdynamic -fno-omit-frame-pointer)
+add_compiler_flags(RELEASE -O2)
 
 ###############################################################################
 # important C/C++ flags
 ###############################################################################
 
 # Desura uses the awesome new C++ Standard
-add_definitions(-std=c++0x)
+set(CMAKE_CXX_FLAGS -std=c++0x)
 
 # Karol Herbst: I get an error while building without that
 add_definitions(-fpermissive)
