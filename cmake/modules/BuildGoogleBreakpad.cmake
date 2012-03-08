39f282e829c6b1fc9bce0872c0a3123316e2d8f8
Karol Herbst <git@karolherbst.de>
2012-03-08 14:11:11 +0100

    no in source build for breakpad

--- cmake/modules/BuildGoogleBreakpad.cmake
+++ cmake/modules/BuildGoogleBreakpad.cmake
@@ -1,13 +1,14 @@
+set(BREAKPAD_EXCEPTION_HANDLER_INSTALL_DIR ${CMAKE_EXTERNAL_BINARY_DIR}/breakpad)
+
 ExternalProject_Add(
     breakpad
     SVN_REPOSITORY http://google-breakpad.googlecode.com/svn/trunk -r 699
     UPDATE_COMMAND ""
     PATCH_COMMAND patch -p0 -N -i ${CMAKE_SOURCE_DIR}/src/third_party/patches/breakpad_patch.patch
     CONFIGURE_COMMAND <SOURCE_DIR>/configure --enable-shared=no --enable-static=yes
-        --prefix=${CMAKE_SOURCE_DIR}/src/third_party/breakpad
+        --prefix=${BREAKPAD_EXCEPTION_HANDLER_INSTALL_DIR}
 )
 
-set(BREAKPAD_EXCEPTION_HANDLER_INSTALL_DIR ${CMAKE_SOURCE_DIR}/src/third_party/breakpad)
 set(BREAKPAD_EXCEPTION_HANDLER_LIBRARY ${BREAKPAD_EXCEPTION_HANDLER_INSTALL_DIR}/lib)
 set(BREAKPAD_EXCEPTION_HANDLER_INCLUDE_DIR ${BREAKPAD_EXCEPTION_HANDLER_INSTALL_DIR}/include)
 set(BREAKPAD_EXCEPTION_HANDLER_LIBRARIES "${BREAKPAD_EXCEPTION_HANDLER_LIBRARY}/libbreakpad.a;${BREAKPAD_EXCEPTION_HANDLER_LIBRARY}/libbreakpad_client.a")
