c0e8f8c058036bc4f401bee100fb01faf7aad6c7
Karol Herbst <git@karolherbst.de>
2012-03-08 14:11:46 +0100

    no in source build for wxWidgets

--- cmake/modules/BuildwxWidgets.cmake
+++ cmake/modules/BuildwxWidgets.cmake
@@ -1,3 +1,5 @@
+set(wxWidgets_INSTALL_DIR ${CMAKE_EXTERNAL_BINARY_DIR}/wxWidgets)
+
 ExternalProject_Add(
     wxWidget-2-9
     SVN_REPOSITORY http://svn.wxwidgets.org/svn/wx/wxWidgets/tags/WX_2_9_0
@@ -5,10 +7,9 @@ ExternalProject_Add(
     PATCH_COMMAND patch -p0 -N -i ${CMAKE_SOURCE_DIR}/src/third_party/patches/wxWidgets_patch.patch
     CONFIGURE_COMMAND <SOURCE_DIR>/configure --enable-shared --enable-unicode
         --enable-monolithic --with-flavour=desura --disable-threads --with-opengl=no
-        --disable-joystick --disable-mediactrl --prefix=${CMAKE_SOURCE_DIR}/src/third_party/wxWidgets
+        --disable-joystick --disable-mediactrl --prefix=${wxWidgets_INSTALL_DIR}
 )
 
-set(wxWidgets_INSTALL_DIR ${CMAKE_SOURCE_DIR}/src/third_party/wxWidgets)
 set(wxWidgets_BIN_DIRS ${wxWidgets_INSTALL_DIR}/bin)
 set(wxWidgets_LIBRARY_DIRS ${wxWidgets_INSTALL_DIR}/lib)
 set(wxWidgets_INCLUDE_DIRS ${wxWidgets_INSTALL_DIR}/include/wx-2.9-desura ${wxWidgets_LIBRARY_DIRS}/wx/include/gtk2-unicode-release-2.9-desura)
