cd836d3d99d39288a3214c18677529e6b912fed7
Karol Herbst <git@karolherbst.de>
2012-03-08 13:49:03 +0100

    create SetInstallPaths.cmake

---
+++ cmake/modules/SetInstallPaths.cmake
@@ -0,0 +1,5 @@
+# set this with cmake -DCMAKE_INSTALL_PREFIX=/where/to/install
+set(INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
+set(BIN_INSTALL_DIR ${INSTALL_DIR}/bin)
+set(LIB_INSTALL_DIR ${INSTALL_DIR}/lib)
+set(DATA_INSTALL_DIR ${INSTALL_DIR}/data)
