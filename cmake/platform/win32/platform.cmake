1184e0926301fdd21190e35cd8622564a1026c29
Karol Herbst <git@karolherbst.de>
2012-03-09 15:57:29 +0100

    finding boost on windows

--- cmake/platform/win32/platform.cmake
+++ cmake/platform/win32/platform.cmake
@@ -1,1 +1,8 @@
-error("Windows is not supported by cmake yet")
+################################################################################
+# set some windows specific variables
+################################################################################
+
+# some boost options
+set(Boost_USE_MULTITHREADED      ON)
+set(Boost_USE_STATIC_LIBS        ON)
+set(Boost_USE_STATIC_RUNTIME     ON)
