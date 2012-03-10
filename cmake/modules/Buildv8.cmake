56d5f11084037990c970e108bf34694ceca4ff59
Karol Herbst <git@karolherbst.de>
2012-03-10 20:32:01 +0100

    added v8 build script

---
+++ cmake/modules/Buildv8.cmake
@@ -0,0 +1,25 @@
+if(BITNESS EQUAL 64)
+  set(EXTRA_OPTS "arch=x64")
+endif()
+
+ExternalProject_Add(
+    v8
+    SVN_REPOSITORY http://v8.googlecode.com/svn/tags/3.7.7/
+    DOWNLOAD_DIR ${V8_INSTALL_DIR}
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND scons --clean
+    BUILD_COMMAND scons snapshot=on mode=release library=shared ${EXTRA_OPTS}
+    BUILD_IN_SOURCE 1
+    INSTALL_COMMAND ""
+)
+
+ExternalProject_Get_Property(
+    v8
+    source_dir
+)
+
+set(V8_INSTALL_DIR ${source_dir})
+set(V8_INCLUDE_DIR ${V8_INSTALL_DIR}/include)
+set(V8_LIBRARIES "${V8_INSTALL_DIR}/libv8.so;${V8_INSTALL_DIR}/libv8preparser.so")
+install(FILES ${V8_LIBRARIES}
+        DESTINATION ${LIB_INSTALL_DIR})
--- cmake/modules/CheckCompiler.cmake
+++ cmake/modules/CheckCompiler.cmake
@@ -1,3 +1,11 @@
+set(BITNESS 32)
+if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+  set(BITNESS 64)
+  message("-- detected 64bit")
+else()
+  message("-- detected 32bit")
+endif()
+
 if(UNIX)
   include(${CMAKE_PLATFORM_PATH}/unix/platform.cmake)
 elseif(WIN32)
