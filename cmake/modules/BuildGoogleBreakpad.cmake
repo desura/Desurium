dc681d51b7fbb93c02a0fa95dd3622c3bb7c9a28
Karol Herbst <git@karolherbst.de>
2012-03-11 12:57:50 +0100

    breakpad: add configure script for windows

--- cmake/modules/BuildGoogleBreakpad.cmake
+++ cmake/modules/BuildGoogleBreakpad.cmake
@@ -1,11 +1,16 @@
 set(BREAKPAD_EXCEPTION_HANDLER_INSTALL_DIR ${CMAKE_EXTERNAL_BINARY_DIR}/breakpad)
 
+if(WIN32)
+  set(CONFIGURE_COMMAND ${PYTHON_EXECUTABLE} <SOURCE_DIR>/src/tools/gyp/gyp <SOURCE_DIR>/src/client/windows/breakpad_client.gyp)
+else()
+  set(CONFIGURE_COMMAND <SOURCE_DIR>/configure --enable-shared=no --enable-static=yes --prefix=${BREAKPAD_EXCEPTION_HANDLER_INSTALL_DIR})
+endif()
+
 ExternalProject_Add(
     breakpad
     SVN_REPOSITORY http://google-breakpad.googlecode.com/svn/trunk -r 699
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND <SOURCE_DIR>/configure --enable-shared=no --enable-static=yes
-        --prefix=${BREAKPAD_EXCEPTION_HANDLER_INSTALL_DIR}
+    CONFIGURE_COMMAND ${CONFIGURE_COMMAND}
 )
 
 set(BREAKPAD_EXCEPTION_HANDLER_LIBRARY ${BREAKPAD_EXCEPTION_HANDLER_INSTALL_DIR}/lib)
