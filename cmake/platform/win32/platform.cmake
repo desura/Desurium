c4add20a8178d16625992c8e8fdc5eae27038ef8
Karol Herbst <git@karolherbst.de>
2012-03-12 10:55:18 +0100

    some windows options

--- cmake/platform/win32/platform.cmake
+++ cmake/platform/win32/platform.cmake
@@ -6,3 +6,7 @@
 set(Boost_USE_MULTITHREADED      ON)
 set(Boost_USE_STATIC_LIBS        ON)
 set(Boost_USE_STATIC_RUNTIME     ON)
+
+add_definitions(/ignore:4006
+                /ignore:4099
+                /ignore:4221)
