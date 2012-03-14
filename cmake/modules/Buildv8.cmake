8176557c06e85e54564f82ffc2ff1632e29bee42
Karol Herbst <git@karolherbst.de>
2012-03-14 12:00:17 +0100

    v8 build on windows

--- cmake/modules/Buildv8.cmake
+++ cmake/modules/Buildv8.cmake
@@ -2,15 +2,24 @@ if(BITNESS EQUAL 64)
   set(EXTRA_OPTS "arch=x64")
 endif()
 
+set(V8_SVN http://v8.googlecode.com/svn/tags/3.7.7/)
+
+if(WIN32)
+  set(SCONS_COMMAND ${PYTHON_INCLUDE_DIRS}/../Scripts/scons.bat snapshot=on mode=release msvcrt=static ${EXTRA_OPTS})
+  set(V8_LIB_SUFFIX lib)
+else()
+  set(SCONS_COMMAND scons snapshot=on mode=release library=shared ${EXTRA_OPTS})
+  set(V8_LIB_SUFFIX so)
+endif()
+
 ExternalProject_Add(
-    v8
-    SVN_REPOSITORY http://v8.googlecode.com/svn/tags/3.7.7/
-    DOWNLOAD_DIR ${V8_INSTALL_DIR}
-    UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND scons snapshot=on mode=release library=shared ${EXTRA_OPTS}
-    BUILD_IN_SOURCE 1
-    INSTALL_COMMAND ""
+  v8
+  SVN_REPOSITORY ${V8_SVN}
+  UPDATE_COMMAND ""
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ${SCONS_COMMAND}
+  BUILD_IN_SOURCE 1
+  INSTALL_COMMAND ""
 )
 
 ExternalProject_Get_Property(
@@ -20,6 +29,6 @@ ExternalProject_Get_Property(
 
 set(V8_INSTALL_DIR ${source_dir})
 set(V8_INCLUDE_DIR ${V8_INSTALL_DIR}/include)
-set(V8_LIBRARIES "${V8_INSTALL_DIR}/libv8.so;${V8_INSTALL_DIR}/libv8preparser.so")
+set(V8_LIBRARIES "${V8_INSTALL_DIR}/libv8.${V8_LIB_SUFFIX};${V8_INSTALL_DIR}/libv8preparser.${V8_LIB_SUFFIX}")
 install(FILES ${V8_LIBRARIES}
         DESTINATION ${LIB_INSTALL_DIR})
