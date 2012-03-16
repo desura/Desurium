bf82e8435e6d7c6f5a2abf4ea6302fe2c710d6b9
matthiaskrgr <matthias.krueger@famsik.de>
2012-03-17 00:18:28 +0100

    build_cmake_clean.sh: instead of just removing the build folder, cd into then, do "make clean" and revert patches. This way we won't have to re-download all sources if we want to make a clean build.

--- build_cmake_clean.sh
+++ build_cmake_clean.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-rm -rf build
+echo "reverting patches..."
+cd ./build/wxWidget-2-9-prefix/src/wxWidget-2-9
+svn revert */*/*/* */*/*
+cd ../../../../build
+echo "making clean"
+make clean
+echo "removing install directory"
 rm -rf install
