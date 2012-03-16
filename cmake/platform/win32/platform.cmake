79e889b37dd55f1cbf6f73f631bc59abcf56dd12
Karol Herbst <git@karolherbst.de>
2012-03-16 16:57:08 +0100

    unicode support must be activated with the use_unicode_here macro (windows only)

--- cmake/platform/win32/platform.cmake
+++ cmake/platform/win32/platform.cmake
@@ -6,11 +6,13 @@
 set(Boost_USE_MULTITHREADED      ON)
 set(Boost_USE_STATIC_LIBS        ON)
 set(Boost_USE_STATIC_RUNTIME     ON)
-add_definitions(/DBOOST_FILESYSTEM_VERSION=2)
+add_definitions(-DBOOST_FILESYSTEM_VERSION=2)
 
 # some wxWidget options
-add_definitions(/D_UNICODE)
-add_definitions(/DwxUSE_UNICODE)
+macro(use_unicode_here)
+  add_definitions(-D_UNICODE)
+  add_definitions(-DwxUSE_UNICODE)
+endmacro()
 
 # from official repo, do we need them?
 add_definitions(/ignore:4006
