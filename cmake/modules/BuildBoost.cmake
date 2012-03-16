6db27873e043d09909f3aed8f5cc7e73b02b2ab2
Karol Herbst <git@karolherbst.de>
2012-03-16 23:51:01 +0100

    fix boost compile

--- cmake/modules/BuildBoost.cmake
+++ cmake/modules/BuildBoost.cmake
@@ -9,7 +9,7 @@ ExternalProject_Add(
   UPDATE_COMMAND ""
   BUILD_IN_SOURCE 1
   CONFIGURE_COMMAND bootstrap.bat
-  BUILD_COMMAND b2.exe --layout=system --with-date_time --with-filesystem
+  BUILD_COMMAND b2.exe --with-date_time --with-filesystem
                   --with-thread --with-system variant=release link=static
 				  threading=multi runtime-link=shared
   INSTALL_COMMAND ""
@@ -24,4 +24,6 @@ set(Boost_DIR ${source_dir})
 set(Boost_INCLUDE_DIR ${Boost_DIR})
 set(Boost_LIBRARY_DIR ${Boost_DIR}/stage/lib)
 
-set(Boost_LIBRARIES "${Boost_LIBRARY_DIR}/libboost_date_time.lib;${Boost_LIBRARY_DIR}/libboost_filesystem.lib;${Boost_LIBRARY_DIR}/libboost_system.lib;${Boost_LIBRARY_DIR}/libboost_thread.lib")
+# todo: get desurium build with system layout
+set(BAS "-vc100-mt-1_49")
+set(Boost_LIBRARIES "${Boost_LIBRARY_DIR}/libboost_date_time${BAS}.lib;${Boost_LIBRARY_DIR}/libboost_filesystem${BAS}.lib;${Boost_LIBRARY_DIR}/libboost_system${BAS}.lib;${Boost_LIBRARY_DIR}/libboost_thread${BAS}.lib")
