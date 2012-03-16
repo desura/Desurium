a3df07b58ca8d91d6deb82b52772e2b564346bf4
matthiaskrgr <matthias.krueger@famsik.de>
2012-03-17 00:20:19 +0100

    build_cmake_clean.sh: if cd fails, abort the script

--- build_cmake_clean.sh
+++ build_cmake_clean.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "reverting patches..."
-cd ./build/wxWidget-2-9-prefix/src/wxWidget-2-9
+cd ./build/wxWidget-2-9-prefix/src/wxWidget-2-9 || exit 1
 svn revert */*/*/* */*/*
-cd ../../../../build
+cd ../../../../build || exit 2
 echo "making clean"
 make clean
 echo "removing install directory"
