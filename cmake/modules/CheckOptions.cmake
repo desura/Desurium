6bc613a96a389322bfdffb79a73c1eaebf5e0d29
Karol Herbst <git@karolherbst.de>
2012-03-31 10:03:10 +0200

    revert renaming desura to desurium in README readded CACHE STRING branding

--- cmake/modules/CheckOptions.cmake
+++ cmake/modules/CheckOptions.cmake
@@ -5,6 +5,14 @@
 option(DEBUG "enable debug build" OFF)
 option(OFFICIAL_BUILD "do you want to compile an official build?" OFF)
 
+if(OFFICIAL_BUILD)
+  set(BRANDING "branding_desura"
+      CACHE STRING "The branding to use for Desura.")
+else()
+  set(BRANDING "branding_desurium"
+      CACHE STRING "The branding to use for Desura.")
+endif()
+
 ###############################################################################
 # unix-only options
 ###############################################################################
