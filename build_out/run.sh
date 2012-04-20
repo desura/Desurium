544257e5498b9ed584d0555019cce1bc40667d12
Jookia <166291@gmail.com>
2012-04-20 10:31:01 +1000

    run.sh now works outside the install directory (symlink it to /usr/bin/desura for fun!)

--- build_out/run.sh
+++ build_out/run.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+# Stolen from http://stackoverflow.com/a/246128/292831
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd $DIR
+
 export PATH="`pwd`:$PATH"
 export LD_LIBRARY_PATH="`pwd`/lib"
 desura
