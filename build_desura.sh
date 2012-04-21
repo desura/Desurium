ce9c76876699eecff269220f43e42d3f6eb7e9b5
Stephan Sokolow <from_github@ssokolow.com>
2012-04-21 04:40:49 -0300

    Use `printf` to display colorized messages in build_desura.sh
    Fixes #168

--- build_desura.sh
+++ build_desura.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo -e "Make sure to run \e[1;31msudo install-deps.sh\e[0m before compiling!"
+printf  'Make sure to run \033[1;31msudo install-deps.sh\033[0m before compiling!\n'
 if [ ! -d "build" ] ; then
  mkdir build
 fi
 cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=../install || exit 1
 make install $@ || exit 2
-echo -e "Run \e[1;31m./install/run.sh\e[0m to start Desura!"
+printf 'Run \033[1;31m./install/run.sh\033[0m to start Desura!\n'
