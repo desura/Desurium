4329022aec0e09d46ee15756d807d966f8c47337
Karol Herbst <git@karolherbst.de>
2012-04-24 13:57:45 +0200

    lets give a try

--- cmake/modules/BuildwxWidgets.cmake
+++ cmake/modules/BuildwxWidgets.cmake
@@ -59,7 +59,7 @@ else()
     UPDATE_COMMAND ""
     PATCH_COMMAND patch -p0 -N -i ${CMAKE_SOURCE_DIR}/cmake/patches/wxWidgets.patch
     CONFIGURE_COMMAND <SOURCE_DIR>/configure --enable-shared --enable-unicode ${CONFIGURE_DEBUG}
-        --enable-monolithic --with-flavour=desura --disable-threads --with-opengl=no --disable-palette
+        --enable-monolithic --with-flavour=desura --enable-threads --with-opengl=no --disable-palette
         --disable-joystick --disable-mediactrl --prefix=${wxWidgets_INSTALL_DIR} --enable-permissive
   )
   
