c407460e2ca268e3f574c45e3194be93fb9238c8
Karol Herbst <git@karolherbst.de>
2012-04-24 11:53:40 +0200

    fix my issue from 169

--- build_out/run.sh
+++ build_out/run.sh
@@ -5,8 +5,6 @@ SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-cd $DIR
-
-export PATH="`pwd`:$PATH"
-export LD_LIBRARY_PATH="`pwd`/lib"
+export PATH="$DIR:$PATH"
+export LD_LIBRARY_PATH="$DIR/lib"
 desura
