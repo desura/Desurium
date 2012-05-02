7c3bf476bddf5177da890612d4f624c055dde981
Karol Herbst <git@karolherbst.de>
2012-05-02 18:24:15 +0200

    cmake: you can write an own cmake file for each compiler on UNIX based systems, so we can use different flags for different compilers

---
+++ cmake/platform/unix/compiler/GNU.cmake
@@ -0,0 +1,5 @@
+message("-- using GCC")
+add_compiler_flags(-fPIC -pipe -fvisibility=hidden -Wl,-Bsymbolic-functions -lpthread -finline-functions)
+add_compiler_flags(DEBUG -rdynamic -fno-omit-frame-pointer -g3)
+add_compiler_flags(RELEASE -O2)
+add_compiler_flags(CXX -fpermissive -std=c++0x)
--- cmake/platform/unix/platform.cmake
+++ cmake/platform/unix/platform.cmake
@@ -9,7 +9,6 @@ add_definitions(-DBOOST_FILESYSTEM_VERSION=2 -D_LARGE_FILES
 # some compiler flags
 ###############################################################################
 
-# TODO: if DEBUG is not defined, the binary will be copied
 if(DEBUG)
     add_definitions(-DDEBUG)
 else()
@@ -25,15 +24,6 @@ else()
   set(CONFIGURE_DEBUG --disable-debug)
   set(MODE_DEBUG mode=release)
 endif()
-
-add_compiler_flags(-fPIC -pipe -fvisibility=hidden -Wl,-Bsymbolic-functions -lpthread -finline-functions)
-add_compiler_flags(DEBUG -rdynamic -fno-omit-frame-pointer -g3)
-add_compiler_flags(RELEASE -O2)
-
-add_compiler_flags(CXX -fpermissive)
-
-# Desura uses the awesome new C++ Standard
-add_compiler_flags(CXX -std=c++0x)
 
 # wxWidgets config
 add_definitions(-D__WXGTK__)
