0e0187ab39c1c53a63fe90ae41e1d2d5ec97cc75
makson96 <makson96@gmail.com>
2012-06-11 10:00:07 +0300

    Update Debian related variables.

--- cmake/modules/Package.cmake
+++ cmake/modules/Package.cmake
@@ -12,6 +12,7 @@ set(CPACK_SOURCE_GENERATOR "TBZ2;TGZ;ZIP")
 ###############################################################################
 
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Desura is a gaming client that allows users to one click download and install games and game modification.")
+set(CPACK_PACKAGE_HOMEPAGE "http://www.desura.com/")
 set(CPACK_PACKAGE_VERSION_MAJOR 0)
 set(CPACK_PACKAGE_VERSION_MINOR 8)
 set(CPACK_PACKAGE_VERSION_PATCH 0)
@@ -31,10 +32,13 @@ set(CPACK_SOURCE_PACKAGE_FILE_NAME "${PROJECT_NAME}-${CPACK_PACKAGE_VERSION}")
 # deb specific values
 ###############################################################################
 
+set(CPACK_DEBIAN_PACKAGE_NAME "desurium")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Karol Herbst")
-set(CPACK_DEBIAN_PACKAGE_DEPENDS "flex gperf xdg-utils yasm")
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libboost-filesystem-dev libboost-thread-dev libc-ares2 libjpeg62 libtinyxml-dev libwebp-dev")
+set(CPACK_DEBIAN_PACKAGE_SECTION "games")
+set(CPACK_DEBIAN_PACKAGE_PRIORITY "extra")
+set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "${CPACK_PACKAGE_HOMEPAGE}"
 set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "${CPACK_PACKAGE_DESCRIPTION_SUMMARY}")
-set(CPACK_DEBIAN_PACKAGE_NAME "desura")
 
 ###############################################################################
 # rpm specific values
