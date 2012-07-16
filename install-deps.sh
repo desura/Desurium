8499ebf1b5316e172f8c68e64adbadc1bbec4013
tomaszmakarewicz <tomaszm@biotech.ug.gda.pl>
2012-07-16 13:37:19 +0200

    Solve Issue #247

--- install-deps.sh
+++ install-deps.sh
@@ -7,20 +7,23 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 if [ -f /etc/debian_version ]; then # Debian  (untested!)
-    DEPS="autoconf automake binutils bison build-essential cmake flex gcc gperf libasound2-dev libboost-dev libboost-date-time-dev libboost-filesystem-dev libboost-system-dev libboost-test-dev libboost-thread-dev libbz2-dev libc-ares-dev libcups2-dev libdbus-glib-1-dev libevent-dev libflac-dev libgconf2-dev libgnome-keyring-dev libgtk2.0-dev libjpeg62-dev libnotify-dev libnss3-dev libpng12-dev libspeex-dev libsqlite3-dev libssl-dev libtinyxml-dev libtool libv8-dev libx11-dev libxml2-dev libxpm-dev libxslt1-dev m4 scons xdg-utils yasm"
-    ST=`dpkg -l ${DEPS} 2>/dev/null | grep "^ii\ "  | cut -d' ' -f2`
-    for PACK in ${DEPS}
-    do
-        CHECK=`echo "${ST}" | grep ${PACK}`
-        if [ "${CHECK}" = "" ] ; then
-            MUST_BE_INSTALLED="${MUST_BE_INSTALLED} ${PACK}"
-        fi
-    done
-    if [ "${MUST_BE_INSTALLED}" != "" ] ; then
-        apt-get install ${MUST_BE_INSTALLED}
-    else
-        echo "everything installed"
-    fi
+	DEPS="autoconf automake binutils bison build-essential cmake flex gcc gperf libasound2-dev libboost-dev libboost-date-time-dev libboost-filesystem-dev libboost-system-dev libboost-test-dev libboost-thread-dev libbz2-dev libc-ares-dev libcups2-dev libdbus-glib-1-dev libevent-dev libflac-dev libgconf2-dev libgnome-keyring-dev libgtk2.0-dev libjpeg62-dev libnotify-dev libnss3-dev libpng12-dev libspeex-dev libsqlite3-dev libssl-dev libtinyxml-dev libtool libv8-dev libx11-dev libxml2-dev libxpm-dev libxslt1-dev m4 scons xdg-utils yasm"
+	if [ `uname -m` == 'x86_64' ]; then # 64 bit dependencies for 32BIT_SUPPORT
+		DEPS=$DEPS" libc6-dev-i386 gcc-multilib"
+	fi
+	ST=`dpkg -l ${DEPS} 2>/dev/null | grep "^ii\ "  | cut -d' ' -f2`
+	for PACK in ${DEPS}
+	do
+		CHECK=`echo "${ST}" | grep ${PACK}`
+		if [ "${CHECK}" = "" ] ; then
+			MUST_BE_INSTALLED="${MUST_BE_INSTALLED} ${PACK}"
+		fi
+	done
+	if [ "${MUST_BE_INSTALLED}" != "" ] ; then
+		apt-get install ${MUST_BE_INSTALLED}
+	else
+		echo "everything installed"
+	fi
 elif [ -f /etc/redhat-release ]; then
 	yum install m4 autoconf gcc-c++ libstdc++-static glibc-devel binutils autoconf libtool gtk2-devel nss-devel GConf2-devel libgnome-keyring-devel dbus-glib-devel gperf bison cups-devel flex libjpeg-turbo-devel alsa-lib-devel bzip2-devel libXpm-devel libX11-devel openssl-devel libnotify-devel scons xdg-user-dirs v8-devel c-ares-devel sqlite-devel libxslt-devel yasm-devel libevent-devel boost-devel boost-static 
 elif [ -f /etc/arch-release ]; then
@@ -35,7 +38,7 @@ elif [ -f /etc/arch-release ]; then
 		pacman -S --asdeps ${DEPS}
 	fi
 elif [ -f /usr/bin/emerge ]; then # Gentoo
-    echo "Please copy the ebuild under gentoo/portage into a local overlay and emerge via emerge --onlydeps desura"
+	echo "Please copy the ebuild under gentoo/portage into a local overlay and emerge via emerge --onlydeps desura"
 else # else
 	echo "Unsupported operating system."
 fi
