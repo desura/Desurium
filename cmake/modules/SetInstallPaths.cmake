35cf4a82e32078f97b3be0787c146a45ab4beca6
Karol Herbst <git@karolherbst.de>
2012-08-29 17:12:07 +0200

    cmake: allow the data folder to be installed everywhere
    this commit also introduce three new cmake options: BINDIR, DATADIR and LIBDIR
    also there is a new cmake variable for source file generating: DATA_INSOURCE_DIR
    this variable will be set as a relative value, if DATADIR is a relative path
    and it will be a absolute path, if DATADIR is absolute
    it will always be relative to BINDIR, so we don't have to change much source inside desurium

--- cmake/modules/SetInstallPaths.cmake
+++ cmake/modules/SetInstallPaths.cmake
@@ -1,18 +1,36 @@
-set(UNIX_DEFAULT_INSTALL_DIR "/usr/share/desura")
-set(WIN_DEFAULT_INSTALL_DIR "")
-
 if(UNIX)
-  set(DEFAULT_INSTALL_DIR ${UNIX_DEFAULT_INSTALL_DIR})
+  set(DEFAULT_INSTALL_DIR "/usr/local")
 elseif(WIN32)
-  set(DEFAULT_INSTALL_DIR ${WIN_DEFAULT_INSTALL_DIR})
+  set(DEFAULT_INSTALL_DIR "")
 endif()
 
 set(CMAKE_INSTALL_PREFIX ${DEFAULT_INSTALL_DIR}
     CACHE STRING "Desura Install Prefix")
+set(BINDIR "./bin"
+    CACHE STRING "Desura Bin Install Dir")
+set(LIBDIR "./bin/lib"
+    CACHE STRING "Desura Lib Dir")
+set(DATADIR "./share/desura"
+    CACHE STRING "Desura Data Install Dir")
 
-# set this with cmake -DCMAKE_INSTALL_PREFIX=/where/to/install
-set(INSTALL_DIR ${CMAKE_INSTALL_PREFIX})
-set(BIN_INSTALL_DIR ${INSTALL_DIR})
-set(LIB_INSTALL_DIR ${INSTALL_DIR}/lib)
-set(DATA_INSTALL_DIR ${INSTALL_DIR})
+# set variables used by cmake
+if(IS_ABSOLUTE ${BINDIR})
+  set(BIN_INSTALL_DIR ${BINDIR})
+else()
+  set(BIN_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/${BINDIR})
+endif()
+
+if(IS_ABSOLUTE ${LIBDIR})
+  set(LIB_INSTALL_DIR ${LIBDIR})
+else()
+  set(LIB_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/${LIBDIR})
+endif()
+
+if(IS_ABSOLUTE ${DATADIR})
+  set(DATA_INSTALL_DIR ${DATADIR})
+  set(DATA_INSOURCE_DIR ${DATADIR})
+else()
+  set(DATA_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/${DATADIR})
+  file(RELATIVE_PATH DATA_INSOURCE_DIR ${BIN_INSTALL_DIR} ${DATA_INSTALL_DIR})
+endif()
 
