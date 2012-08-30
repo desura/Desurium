629ed25c51ef513d7bdc7216aef51db50560c8f3
Karol Herbst <git@karolherbst.de>
2012-08-30 08:22:33 +0200

    cmake: update build_desura.sh for FHS changes

--- build_desura.sh
+++ build_desura.sh
@@ -4,6 +4,6 @@ if [ ! -d "build" ] ; then
  mkdir build
 fi
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=../install || exit 1
+cmake .. -DCMAKE_INSTALL_PREFIX=../install -DBINDIR="" -DRUNTIME_LIBDIR="lib" -DDATADIR="" || exit 1
 make install $@ || exit 2
-printf 'Run \033[1;31m./install/run.sh\033[0m to start Desura!\n'
+printf 'Run \033[1;31m./install/desura\033[0m to start Desura!\n'
