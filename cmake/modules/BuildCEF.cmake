50c0944682f4a8ef4f861a484428b896db13fe3f
Karol Herbst <git@karolherbst.de>
2012-08-31 18:02:02 +0200

    cmake: extract wx-config part out of the main cef.patch
    in the next step I will configure the path to wx-config via cmake

--- cmake/modules/BuildCEF.cmake
+++ cmake/modules/BuildCEF.cmake
@@ -50,6 +50,19 @@ ExternalProject_Add(
   INSTALL_COMMAND ""
 )
 
+ExternalProject_Get_Property(
+  fetch_cef
+  source_dir
+)
+set(FETCH_CEF_SOURCE_DIR ${source_dir})
+
+ExternalProject_Add_Step(
+  fetch_cef
+  cef_gyp-patch
+  COMMAND ${CMAKE_SCRIPT_PATH}/patch.sh ${CMAKE_SOURCE_DIR}/cmake/patches/cef_gyp.patch
+  DEPENDEES patch
+  WORKING_DIRECTORY ${FETCH_CEF_SOURCE_DIR}
+)
 
 ExternalProject_Add(
   cef
