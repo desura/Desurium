fbca6e97338dc9f613e2201a0aea35cc429773be
Karol Herbst <git@karolherbst.de>
2012-09-11 18:31:59 +0200

    cmake: add posibility to build cef separatly from desurium

--- cmake/modules/CheckOptions.cmake
+++ cmake/modules/CheckOptions.cmake
@@ -2,53 +2,57 @@
 # global options
 ###############################################################################
 
-option(BUILD_TESTS "Build various unit tests." ON)
-if(BUILD_TESTS)
-  include(CTest)
-  enable_testing()
-endif()
+option(BUILD_ONLY_CEF "build only cef, this will disable desurium building" OFF)
 
-option(DEBUG "enable debug build" OFF)
-option(OFFICIAL_BUILD "do you want to compile an official build?" OFF)
-option(SET_OWN_EXT_SRC "useful for ebuild or saving traffic" OFF)
-option(BUILD_CEF "Build cef" ON)
+if(NOT BUILD_ONLY_CEF)
+  option(BUILD_TESTS "Build various unit tests." ON)
+  if(BUILD_TESTS)
+    include(CTest)
+    enable_testing()
+  endif()
 
-if(OFFICIAL_BUILD)
-  set(BRANDING "branding_desura"
+  option(DEBUG "enable debug build" OFF)
+  option(OFFICIAL_BUILD "do you want to compile an official build?" OFF)
+  option(SET_OWN_EXT_SRC "useful for ebuild or saving traffic" OFF)
+  option(BUILD_CEF "Build cef" ON)
+
+  if(OFFICIAL_BUILD)
+    set(BRANDING "branding_desura"
       CACHE STRING "The branding to use for Desura.")
-else()
-  set(BRANDING "branding_desurium"
+  else()
+    set(BRANDING "branding_desurium"
       CACHE STRING "The branding to use for Desura.")
-endif()
+  endif()
 
-option(FORCE_SYS_DEPS "Force the use of system libs")
-if(FORCE_SYS_DEPS)
-  set(REQUIRED_IF_OPTION REQUIRED)
-else()
-  unset(REQUIRED_IF_OPTION)
-endif()
+  option(FORCE_SYS_DEPS "Force the use of system libs")
+  if(FORCE_SYS_DEPS)
+    set(REQUIRED_IF_OPTION REQUIRED)
+  else()
+    unset(REQUIRED_IF_OPTION)
+  endif()
 
-###############################################################################
-# unix-only options
-###############################################################################
+  ###############################################################################
+  # unix-only options
+  ###############################################################################
 
-if(UNIX)
-  option(WITH_ARES "build cURL with c-ares support" ON)
-  option(DEBUG_EXTERNAL "build external libs with debug support" OFF)
-  option(INSTALL_DESKTOP_FILE "install the generated desktop file to /usr/share/applications/" OFF)
-endif()
+  if(UNIX)
+    option(WITH_ARES "build cURL with c-ares support" ON)
+    option(DEBUG_EXTERNAL "build external libs with debug support" OFF)
+    option(INSTALL_DESKTOP_FILE "install the generated desktop file to /usr/share/applications/" OFF)
+  endif()
 
-###############################################################################
-# windows-only options
-###############################################################################
+  ###############################################################################
+  # windows-only options
+  ###############################################################################
 
 
-###############################################################################
-# check set options, print warnings, what ever
-###############################################################################
+  ###############################################################################
+  # check set options, print warnings, what ever
+  ###############################################################################
 
-if(NOT WITH_ARES)
-  message("WW building Desura without c-ares is not supported.")
-  message("WW Make sure, that your system installed cURL has ares support.")
-  message("WW Ignore this if you are aware of what you do. See #189 for further information.")
+  if(NOT WITH_ARES)
+    message("WW building Desura without c-ares is not supported.")
+    message("WW Make sure, that your system installed cURL has ares support.")
+    message("WW Ignore this if you are aware of what you do. See #189 for further information.")
+  endif()
 endif()
