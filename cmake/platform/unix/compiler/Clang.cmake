3c34427c1c77da0beae7f29d9353a70edd0fd181
Karol Herbst <git@karolherbst.de>
2012-10-28 18:49:44 +0100

    separate compiler and linker flags
    fixes #336

--- cmake/platform/unix/compiler/Clang.cmake
+++ cmake/platform/unix/compiler/Clang.cmake
@@ -1,9 +1,11 @@
 message("-- using clang")
 add_compiler_flags(-fPIC -pipe -fvisibility=hidden -finline-functions)
 add_compiler_flags(DEBUG -fno-omit-frame-pointer -g3)
+add_compiler_flags(RELEASE -O2)
+add_compiler_flags(CXX -std=c++0x)
+
 if(DEBUG)
   add_linker_flags(-rdynamic)
 endif()
-add_compiler_flags(RELEASE -O2)
-add_compiler_flags(CXX -std=c++0x)
+
 set(CLANG TRUE)
--- cmake/platform/unix/compiler/Intel.cmake
+++ cmake/platform/unix/compiler/Intel.cmake
@@ -1,7 +1,12 @@
-add_compiler_flags(-fPIC -pipe -fvisibility=hidden -Wl,-Bsymbolic-functions -lpthread -finline-functions -gcc)
-add_compiler_flags(DEBUG -rdynamic -fno-omit-frame-pointer -g3)
+add_compiler_flags(-fPIC -pipe -fvisibility=hidden -finline-functions -gcc)
+add_compiler_flags(DEBUG -fno-omit-frame-pointer -g3)
 add_compiler_flags(RELEASE -O2)
 add_compiler_flags(CXX -fpermissive -std=c++0x)
+
+add_linker_flags(-Bsymbolic-functions -lpthread)
+if(DEBUG)
+  add_linker_flags(-rdynamic)
+endif()
 
 #ignore some warnings
 add_compiler_flags(-wd2928,10120)
