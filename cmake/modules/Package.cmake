f1e9429d2db9955c25b253f0e81f43b6614374db
Karol Herbst <git@karolherbst.de>
2012-11-16 13:40:00 +0100

    version: bump to 0.8.0_rc3

--- cmake/modules/Package.cmake
+++ cmake/modules/Package.cmake
@@ -16,7 +16,7 @@ set(CPACK_PACKAGE_VERSION_MAJOR 0)
 set(CPACK_PACKAGE_VERSION_MINOR 8)
 set(CPACK_PACKAGE_VERSION_PATCH 0)
 # used only for builds, not by cpack!
-set(CPACK_PACKAGE_VERSION_ADDITION -devel)
+set(CPACK_PACKAGE_VERSION_ADDITION _rc3)
 
 ###############################################################################
 # some internal variables
