831a6faad89093c9f2c40ba50853641c4e1ac4c3
matthiaskrgr <matthias.krueger@famsik.de>
2013-01-12 00:02:57 +0100

    install-deps.sh: arch: wording changes

--- install-deps.sh
+++ install-deps.sh
@@ -28,7 +28,7 @@ elif [ -f /etc/redhat-release ]; then
 	yum install m4 autoconf gcc-c++ libstdc++-static glibc-devel binutils autoconf libtool gtk2-devel nss-devel GConf2-devel libgnome-keyring-devel dbus-glib-devel gperf bison cups-devel flex libjpeg-turbo-devel alsa-lib-devel bzip2-devel libXpm-devel libX11-devel openssl-devel libnotify-devel scons xdg-user-dirs v8-devel c-ares-devel sqlite-devel libxslt-devel yasm-devel libevent-devel boost-devel boost-static patch perl-Digest-MD5
 elif [ -f /etc/arch-release ]; then
 	echo -e "\e[1;31mArch Linux detected!\e[0m"
-	echo -e "\e[1;31mNote: there is a pkgbuild in ./distro/archlinux/\e[0m"
+	echo -e "\e[1;31mNote: there are PKGBUILDs in ./distro/archlinux/\e[0m"
 	# By using "pacman -T" to find out needed dependencies, we don't get
 	# conflicts if a package we have installed provides one of the dependencies.
 	DEPS=`pacman -T cmake make boost glib2 pkg-config sqlite m4 autoconf gcc glibc binutils autoconf libtool gtk2 nss libgnome-keyring dbus-glib gperf bison cups flex libjpeg-turbo alsa-lib bzip2 libxpm libx11 openssl scons gconf libnotify xdg-user-dirs v8 c-ares sed flac libpng speex zlib xdg-utils libevent libxslt yasm libxml2 libxxf86vm | sed -e 's/\n/ /g'`
