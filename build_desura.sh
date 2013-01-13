bb8499b7d8e89aa82a94a93de816a37860858f26
matthiaskrgr <matthias.krueger@famsik.de>
2013-01-13 22:49:33 +0100

    Conflicts: 	.travis.yml

--- build_desura.sh
+++ build_desura.sh
@@ -5,9 +5,8 @@ BINDIR=""
 LIBDIR="lib"
 DATADIR=""
 
-printf  'Make sure to run \033[1;31msudo ./install-deps.sh\033[0m before compiling!\n'
-
-printf 'We are compiling CEF first'
+printf 'Make sure to run \033[1;31msudo ./install-deps.sh\033[0m before compiling!\n\n'
+printf 'We are compiling CEF first\n'
 if [ ! -d "build_cef" ] ; then
  mkdir build_cef
 fi
@@ -15,8 +14,8 @@ cd build_cef
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DRUNTIME_LIBDIR=$LIBDIR -DBUILD_ONLY_CEF=ON || exit 1
 make install $@ || exit 2
 cd -
-
-printf 'Now we are compiling desurium'
+printf "\n"
+printf 'Now we are compiling desurium\n'
 if [ ! -d "build" ] ; then
  mkdir build
 fi
@@ -24,4 +23,3 @@ cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DBINDIR=$BINDIR -DRUNTIME_LIBDIR=$LIBDIR -DDATADIR=$DATADIR -DBUILD_CEF=OFF || exit 3
 make install $@ || exit 4
 printf 'Run \033[1;31m./install/desura\033[0m to start Desura!\n'
-
