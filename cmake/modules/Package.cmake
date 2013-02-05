58f04c659ce2ecc3d2ae0bc996d8ae1f1072eeb5
Karol Herbst <git@karolherbst.de>
2013-02-05 11:30:37 +0100

    version: 0.8.0_rc5

--- cmake/modules/Package.cmake
+++ cmake/modules/Package.cmake
@@ -16,7 +16,7 @@ set(CPACK_PACKAGE_VERSION_MAJOR 0)
 set(CPACK_PACKAGE_VERSION_MINOR 8)
 set(CPACK_PACKAGE_VERSION_PATCH 0)
 # used only for builds, not by cpack!
-set(CPACK_PACKAGE_VERSION_ADDITION _rc5-devel)
+set(CPACK_PACKAGE_VERSION_ADDITION _rc5)
 
 ###############################################################################
 # some internal variables
