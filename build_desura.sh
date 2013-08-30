839076d3bf29f04e8ff2158e53e15b284daea5aa
Karol Herbst <git@karolherbst.de>
2013-08-30 16:06:48 +0200

    build_script/package: add information about the binary and icon path
    fixes deb installation

--- build_desura.sh
+++ build_desura.sh
@@ -69,7 +69,7 @@ pack() {
 		mkdir build_package
 	fi
 	cd build_package
-	cmake .. -DPACKAGE_TYPE=$PACKAGE -DINSTALL_DESKTOP_FILE=ON -DCMAKE_INSTALL_PREFIX="/opt/desura" || exit
+	cmake .. -DPACKAGE_TYPE=$PACKAGE -DINSTALL_DESKTOP_FILE=ON -DCMAKE_INSTALL_PREFIX="/opt/desura" -DDESKTOP_EXE="/opt/desura/desura" -DDESKTOP_ICON="/opt/desura/desura.png" || exit
 	make package $args
 	if [ $PACKAGE = "DEB" ]; then
 		mv Desura-*.deb ..
