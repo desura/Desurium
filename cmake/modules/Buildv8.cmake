48aa48d2ae46c05d6cbce02d4f3d49eafa2b3159
lodle <mark@moddb.com>
2013-09-16 16:55:41 -0700

    Fixed issue where svn checkout for v8 happened before extract causing files to be deleted

--- cmake/modules/Buildv8.cmake
+++ cmake/modules/Buildv8.cmake
@@ -34,6 +34,7 @@ ExternalProject_Add_Step(
   v8
   cygwin-svn-fetch
   COMMAND ${Subversion_SVN_EXECUTABLE} co http://src.chromium.org/svn/trunk/deps/third_party/cygwin@66844 third_party/cygwin
+  DEPENDEES download
   DEPENDERS configure
   WORKING_DIRECTORY <SOURCE_DIR>
 )
