9945f49c20feaf76d36b9c79ff2fa50e378d25c9
Karol Herbst <git@karolherbst.de>
2013-09-24 10:27:14 +0200

    cmake/boost: tidy up cmake file

--- cmake/modules/BuildBoost.cmake
+++ cmake/modules/BuildBoost.cmake
@@ -8,37 +8,37 @@ else()
   set(BOOST_EXTRA_BUILD_OPTS "")
 endif()
 
+set(BOOST_BJAM_LIBS --with-date_time --with-filesystem --with-thread --with-system --with-test)
+
 if(DEBUG) 
-ExternalProject_Add(
-  boost
-  URL "${BOOST_URL}"
-  URL_MD5 ${BOOST_MD5}
-  UPDATE_COMMAND ""
-  BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND ${CONFIGURE_COMMAND} ${BOOST_EXTRA_BUILD_OPTS}
-  BUILD_COMMAND ${BJAM_BINARY} --layout=system --with-date_time --with-filesystem
-                  --with-thread --with-system --with-test variant=debug link=static
-				  threading=multi runtime-link=shared
-  INSTALL_COMMAND ""
-)
+  ExternalProject_Add(
+    boost
+    URL "${BOOST_URL}"
+    URL_MD5 ${BOOST_MD5}
+    UPDATE_COMMAND ""
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ${CONFIGURE_COMMAND} ${BOOST_EXTRA_BUILD_OPTS}
+    BUILD_COMMAND ${BJAM_BINARY} ${BOOST_BJAM_LIBS} --layout=system variant=debug link=static
+                    threading=multi runtime-link=shared
+    INSTALL_COMMAND ""
+  )
 else()
-ExternalProject_Add(
-  boost
-  URL "${BOOST_URL}"
-  URL_MD5 ${BOOST_MD5}
-  UPDATE_COMMAND ""
-  BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND ${CONFIGURE_COMMAND} ${BOOST_EXTRA_BUILD_OPTS}
-  BUILD_COMMAND ${BJAM_BINARY} --layout=system --with-date_time --with-filesystem
-                  --with-thread --with-system --with-test variant=release link=static
-				  threading=multi runtime-link=shared 
-  INSTALL_COMMAND ""
-)
+  ExternalProject_Add(
+    boost
+    URL "${BOOST_URL}"
+    URL_MD5 ${BOOST_MD5}
+    UPDATE_COMMAND ""
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ${CONFIGURE_COMMAND} ${BOOST_EXTRA_BUILD_OPTS}
+    BUILD_COMMAND ${BJAM_BINARY} ${BOOST_BJAM_LIBS} --layout=system variant=release link=static
+                    threading=multi runtime-link=shared 
+    INSTALL_COMMAND ""
+  )
 endif()
 
 ExternalProject_Get_Property(
-    boost
-    source_dir
+  boost
+  source_dir
 )
 
 set(Boost_DIR ${source_dir})
@@ -51,15 +51,13 @@ else()
   set(BOOST_SUFFIX lib)
 endif()
 
-set(Boost_CHRONO_LIBRARY "${Boost_LIBRARY_DIR}/libboost_chrono${BAS}.${BOOST_SUFFIX}")
-set(Boost_DATE_TIME_LIBRARY "${Boost_LIBRARY_DIR}/libboost_date_time${BAS}.${BOOST_SUFFIX}")
-set(Boost_FILESYSTEM_LIBRARY "${Boost_LIBRARY_DIR}/libboost_filesystem${BAS}.${BOOST_SUFFIX}")
-set(Boost_PRG_EXEC_MONITOR_LIBRARY "${Boost_LIBRARY_DIR}/libboost_prg_exec_monitor${BAS}.${BOOST_SUFFIX}")
-set(Boost_SYSTEM_LIBRARY "${Boost_LIBRARY_DIR}/libboost_system${BAS}.${BOOST_SUFFIX}")
-set(Boost_TEST_EXEC_MONITOR_LIBRARY "${Boost_LIBRARY_DIR}/libboost_test_exec_monitor${BAS}.${BOOST_SUFFIX}")
-set(Boost_THREAD_LIBRARY "${Boost_LIBRARY_DIR}/libboost_thread${BAS}.${BOOST_SUFFIX}")
-set(Boost_UNIT_TEST_FRAMEWORK_LIBRARY "${Boost_LIBRARY_DIR}/libboost_unit_test_framework${BAS}.${BOOST_SUFFIX}")
-
-set(Boost_LIBRARIES "${Boost_DATE_TIME_LIBRARY};${Boost_FILESYSTEM_LIBRARY};${Boost_SYSTEM_LIBRARY};${Boost_THREAD_LIBRARY};${Boost_PRG_EXEC_MONITOR_LIBRARY};${Boost_TEST_EXEC_MONITOR_LIBRARY};${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}")
+set(Boost_CHRONO_LIBRARY "${Boost_LIBRARY_DIR}/libboost_chrono.${BOOST_SUFFIX}")
+set(Boost_DATE_TIME_LIBRARY "${Boost_LIBRARY_DIR}/libboost_date_time.${BOOST_SUFFIX}")
+set(Boost_FILESYSTEM_LIBRARY "${Boost_LIBRARY_DIR}/libboost_filesystem.${BOOST_SUFFIX}")
+set(Boost_PRG_EXEC_MONITOR_LIBRARY "${Boost_LIBRARY_DIR}/libboost_prg_exec_monitor.${BOOST_SUFFIX}")
+set(Boost_SYSTEM_LIBRARY "${Boost_LIBRARY_DIR}/libboost_system.${BOOST_SUFFIX}")
+set(Boost_TEST_EXEC_MONITOR_LIBRARY "${Boost_LIBRARY_DIR}/libboost_test_exec_monitor.${BOOST_SUFFIX}")
+set(Boost_THREAD_LIBRARY "${Boost_LIBRARY_DIR}/libboost_thread.${BOOST_SUFFIX}")
+set(Boost_UNIT_TEST_FRAMEWORK_LIBRARY "${Boost_LIBRARY_DIR}/libboost_unit_test_framework.${BOOST_SUFFIX}")
 
 set_property(TARGET boost PROPERTY FOLDER "ThirdParty")
