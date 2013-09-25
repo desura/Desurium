2f5a7dc872acf111935207cb0b497bcf476d669b
lodle <mark@moddb.com>
2013-09-25 12:09:02 +0200

    compile some targets with static runtime, too

--- cmake/modules/BuildBoost.cmake
+++ cmake/modules/BuildBoost.cmake
@@ -8,7 +8,8 @@ else()
   set(BOOST_EXTRA_BUILD_OPTS "")
 endif()
 
-set(BOOST_BJAM_LIBS --with-chrono --with-date_time --with-filesystem --with-thread --with-system --with-test)
+set(BOOST_BJAM_LIBS_STATIC --with-chrono --with-filesystem --with-thread --with-system)
+set(BOOST_BJAM_LIBS ${BOOST_BJAM_LIBS_STATIC} --with-date_time --with-test)
 
 if (WIN32 AND NOT MINGW)
   if (MSVC10)
@@ -28,10 +29,11 @@ if(DEBUG)
     UPDATE_COMMAND ""
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ${CONFIGURE_COMMAND} ${BOOST_EXTRA_BUILD_OPTS}
-    BUILD_COMMAND ${BJAM_BINARY} ${BOOST_BJAM_LIBS} --layout=system variant=debug link=static
+    BUILD_COMMAND ${BJAM_BINARY} ${BOOST_BJAM_LIBS} --layout=tagged variant=debug link=static
                     threading=multi runtime-link=shared ${TOOLSET_MSVC_VER}
     INSTALL_COMMAND ""
   )
+  set(BOOST_LIB_ADD_STRING "mt-gd")
 else()
   ExternalProject_Add(
     boost
@@ -40,10 +42,11 @@ else()
     UPDATE_COMMAND ""
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND ${CONFIGURE_COMMAND} ${BOOST_EXTRA_BUILD_OPTS}
-    BUILD_COMMAND ${BJAM_BINARY} ${BOOST_BJAM_LIBS} --layout=system variant=release link=static
+    BUILD_COMMAND ${BJAM_BINARY} ${BOOST_BJAM_LIBS} --layout=tagged variant=release link=static
                     threading=multi runtime-link=shared ${TOOLSET_MSVC_VER}
     INSTALL_COMMAND ""
   )
+  set(BOOST_LIB_ADD_STRING "mt")
 endif()
 
 ExternalProject_Get_Property(
@@ -61,13 +64,54 @@ else()
   set(BOOST_SUFFIX lib)
 endif()
 
-set(Boost_CHRONO_LIBRARY "${Boost_LIBRARY_DIR}/libboost_chrono.${BOOST_SUFFIX}")
-set(Boost_DATE_TIME_LIBRARY "${Boost_LIBRARY_DIR}/libboost_date_time.${BOOST_SUFFIX}")
-set(Boost_FILESYSTEM_LIBRARY "${Boost_LIBRARY_DIR}/libboost_filesystem.${BOOST_SUFFIX}")
-set(Boost_PRG_EXEC_MONITOR_LIBRARY "${Boost_LIBRARY_DIR}/libboost_prg_exec_monitor.${BOOST_SUFFIX}")
-set(Boost_SYSTEM_LIBRARY "${Boost_LIBRARY_DIR}/libboost_system.${BOOST_SUFFIX}")
-set(Boost_TEST_EXEC_MONITOR_LIBRARY "${Boost_LIBRARY_DIR}/libboost_test_exec_monitor.${BOOST_SUFFIX}")
-set(Boost_THREAD_LIBRARY "${Boost_LIBRARY_DIR}/libboost_thread.${BOOST_SUFFIX}")
-set(Boost_UNIT_TEST_FRAMEWORK_LIBRARY "${Boost_LIBRARY_DIR}/libboost_unit_test_framework.${BOOST_SUFFIX}")
+set(Boost_CHRONO_LIBRARY "${Boost_LIBRARY_DIR}/libboost_chrono-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
+set(Boost_DATE_TIME_LIBRARY "${Boost_LIBRARY_DIR}/libboost_date_time-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
+set(Boost_FILESYSTEM_LIBRARY "${Boost_LIBRARY_DIR}/libboost_filesystem-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
+set(Boost_PRG_EXEC_MONITOR_LIBRARY "${Boost_LIBRARY_DIR}/libboost_prg_exec_monitor-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
+set(Boost_SYSTEM_LIBRARY "${Boost_LIBRARY_DIR}/libboost_system-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
+set(Boost_TEST_EXEC_MONITOR_LIBRARY "${Boost_LIBRARY_DIR}/libboost_test_exec_monitor-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
+set(Boost_THREAD_LIBRARY "${Boost_LIBRARY_DIR}/libboost_thread-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
+set(Boost_UNIT_TEST_FRAMEWORK_LIBRARY "${Boost_LIBRARY_DIR}/libboost_unit_test_framework-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
 
 set_property(TARGET boost PROPERTY FOLDER "ThirdParty")
+
+if (WIN32)
+  if(DEBUG)
+    ExternalProject_Add(
+      boost_s
+      UPDATE_COMMAND ""
+      DOWNLOAD_COMMAND ""
+      BUILD_IN_SOURCE 1
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ${BJAM_BINARY} ${BOOST_BJAM_LIBS_STATIC} --layout=tagged
+                      variant=debug link=static threading=multi runtime-link=static ${TOOLSET_MSVC_VER}
+      INSTALL_COMMAND ""
+      SOURCE_DIR "${source_dir}"
+    )
+    set(BOOST_LIB_ADD_STRING "mt-sgd")
+  else()
+    ExternalProject_Add(
+      boost_s
+      UPDATE_COMMAND ""
+      DOWNLOAD_COMMAND ""
+      BUILD_IN_SOURCE 1
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ${BJAM_BINARY} ${BOOST_BJAM_LIBS_STATIC} --layout=tagged
+                      variant=release link=static threading=multi runtime-link=static ${TOOLSET_MSVC_VER}
+      INSTALL_COMMAND ""
+      SOURCE_DIR "${source_dir}"
+    )
+    set(BOOST_LIB_ADD_STRING "mt-s")
+  endif()
+  
+  add_dependencies(boost_s boost)
+
+  set(Boost_LIBRARY_DIR_S ${Boost_DIR_S}/stage/lib)
+
+  set_property(TARGET boost_s PROPERTY FOLDER "ThirdParty")
+
+  set(Boost_CHRONO_LIBRARY_S "${Boost_LIBRARY_DIR}/libboost_chrono-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
+  set(Boost_FILESYSTEM_LIBRARY_S "${Boost_LIBRARY_DIR}/libboost_filesystem-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
+  set(Boost_SYSTEM_LIBRARY_S "${Boost_LIBRARY_DIR}/libboost_system-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
+  set(Boost_THREAD_LIBRARY_S "${Boost_LIBRARY_DIR}/libboost_thread-${BOOST_LIB_ADD_STRING}.${BOOST_SUFFIX}")
+endif()
