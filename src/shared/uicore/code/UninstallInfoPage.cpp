fe03f14f21f4100fa3594275d9d659f7759ea5c5
Lodle <mark@moddb.com>
2013-10-28 12:20:01 -0700

    Fixed uninstall form being to small #689

--- src/shared/uicore/code/UninstallInfoPage.cpp
+++ src/shared/uicore/code/UninstallInfoPage.cpp
@@ -77,7 +77,7 @@ UninstallInfoPage::UninstallInfoPage(wxWindow* parent) : BaseInstallPage(parent)
 
 	this->SetSizer( fgSizer2 );
 	this->Layout();
-	this->setParentSize(-1, 160);
+	this->setParentSize(-1, 240);
 }
 
 UninstallInfoPage::~UninstallInfoPage()
